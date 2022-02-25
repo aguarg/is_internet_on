c306a925f2951d045ab01246249ece87227edbc1
aguarg <93261481+aguarg@users.noreply.github.com>
2022-02-25 18:11:40 -0300

    Finished!
    Corrected the repeating message while checking for connection.

--- alerta_conexion.py
+++ alerta_conexion.py
@@ -1,4 +1,4 @@
-# Modules used:
+# Modules:
 import notify2
 from playsound import playsound
 import time
@@ -7,14 +7,14 @@ from ping3 import ping
 
 
 # ================= FUNCTIONS: ======================== 
-# Function that sends the alert:
+# Function that sends the desktop and sound alerts:
 def notificar():
     # Desktop notification:
     notify2.init('app name')
 
     mensaje = notify2.Notification("Conexión establecida",
                          "Que tengas buen día!",
-                         "notification-message-im"
+                         "notification-message-im" #uses the system icon.
                         )
     mensaje.show()
 
@@ -28,22 +28,31 @@ def notificar():
 
 # Function that checks for internet connection:
 def buscando_conexion(sitio): 
-    print("===========================")
-    print("Chequeando conectividad...")   
-    print("===========================")
-
+    
     respuesta = ping(sitio)
 
     if respuesta == False:
         time.sleep(10)
         buscando_conexion(sitio)
-        
+    
     else:
         notificar()
         print("Conexión disponible! Que tengas un buen día.")
         
 
 
-buscando_conexion("www.google.com")
+# Starting the checking and calling the rest of the functions:
+def iniciar():
+    print("===========================")
+    print("Chequeando conectividad...")   
+    print("===========================")
+
+    buscando_conexion("www.google.com")
+
+
+
+# Calling the main function:
+iniciar()
+
 
 
